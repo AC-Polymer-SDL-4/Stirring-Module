e68a4d0e870a017d80eb928b8d2fe6c9253edd6a
Monique <144367290+mn9102@users.noreply.github.com>
2025-02-26 15:13:14 -0500

    Major changes: turn on, turn off and led indication

--- stirring_module.py
+++ stirring_module.py
@@ -1,52 +1,107 @@
 from machine import Pin, PWM
 import time
 
-def initialize_fans(pin_num, frequency = 1000):
-    """
-    Initialize fan stirring, requires a short period of high PWM to turn on all fans and stir at lower speeds afterwards.
-    
-    Parameters:
-    ---
-    pin_num: the GPIO pin number the fans are connected to
-    frequency: the frequency (Hz) for PWM, how quickly the fan switches between power on/off.
+class StirringModule:
+    PIN_NUM = 0 #GPIO pin number the fans are connected to
+    fans_pwm = ""
+    initialized = False #tracks if fans have been "initialized" (need to stir at high power for 60ms to turn all fans on before stirring)
 
-    Returns:
-    ---
-    PWM object at the designated GPIO pin
-    """
-    fans_pin = Pin(pin_num, Pin.OUT) #GPIO pin for the fan is connected at pin 0
-    fans_pwm = PWM(fans_pin) #initialize PWM at the pin
 
-    fans_pwm.freq(frequency) #set frequency 
-    fans_pwm.duty_u16(65500) #turn on max PWM for 60 milliseconds.
-    time.sleep_ms(60)
+    def __init__(self, frequency = 1000) -> None:
+        """
+        Initialize StirringModule for stirring.
+        
+        Parameters:
+        ---
+        frequency: the frequency (Hz) for PWM, how quickly the fan switches between power on/off.
 
-    return fans_pwm
+        """
+        try:
+            self.fans_pin = Pin(self.PIN_NUM, Pin.OUT)
+            self.fans_pwm = PWM(self.fans_pin) #initialize PWM at the pin
 
-def stir(fans_pwm, power, duration):
-    """
-    Stir using fans at given power(1-100) & duration (in MINUTES)
-    Note: use start() before your first stir function. start() is not needed for changing stirring speed afterwards.
+            self.fans_pwm.freq(frequency) #set frequency 
+            print("Stirring Module initializated")
+        
+        except Exception as e:
+            print("Could not initialize stirring module: ", e)
+            self.stop()
 
-    Parameters:
-    ---
-    power (int): 1-100 (minimum 45 for all fans to turn on)
-    duration (float): stirring duration in MINUTES
-    """
-    if power > 0 and power <= 100 and duration > 0:
-        dutycycle = power/100*65535
-        fans_pwm.duty_u16(int(dutycycle)) #set duty cycle (65535 = 100% power)
-        print(f"Stirring at {power} % power for {duration} minutes")
-        time.sleep(int(duration*60)) #stir for duration in minutes
-    else:
-        print("invalid value for power (0-100) and/or duration(>0)")
-        stop(fans_pwm)
+        
 
-def stop(fans_pwm): #to stop stirring
-    fans_pwm.duty_u16(0) #duty cycle = 0
-    time.sleep_ms(5) #wait for it to stop
+    def initialize_fans(self):
+        """The fans require a short period of high PWM to turn on all fans and stir at lower speeds afterwards. This should be used before stirring."""
+        self.fans_pwm.duty_u16(65500) #turn on max PWM for 60 milliseconds.
+        time.sleep_ms(60)
+        self.initialized = True
 
-def initialize_and_stir(pin_num, power, duration):
+    def stir_for_duration(self, power, duration):
+        """
+        Stir using fans at given power(1-100) & duration (in MINUTES) and then stops.
+
+        Parameters:
+        ---
+        power (int): 1-100 (minimum 45 for all fans to turn on)
+        duration (float): stirring duration in MINUTES
+        """
+        if self.initialized == False:
+            self.initialize_fans()
+            
+        #led = Pin(25, Pin.OUT)
+        #led.value(1) #turn on light 
+
+        if power > 0 and power <= 100 and duration > 0:
+            dutycycle = power/100*65535
+            self.fans_pwm.duty_u16(int(dutycycle)) #set duty cycle (65535 = 100% power)
+            print(f"Stirring at {power}% power for {duration} minutes")
+            time.sleep(int(duration*60)) #stir for duration in minutes
+            self.stop()
+            return True
+        else:
+            print("Invalid value for power (0-100) and/or duration(>0)")
+            self.stop()
+            return False
+
+    def stir(self,power):
+        """
+        Stir using fans at given power(1-100) until stopped using the stop() function
+
+        Parameters:
+        ---
+        power (int): 1-100 (minimum 45 for all fans to turn on)
+        """
+        if self.initialized == False:
+            self.initialize_fans()
+        
+        #led = Pin(25, Pin.OUT)
+        #led.value(1) #turn on light 
+        
+        if power > 0 and power <= 100:
+            dutycycle = power/100*65535
+            self.fans_pwm.duty_u16(int(dutycycle)) #set duty cycle (65535 = 100% power)
+            print(f"Stirring at {power}% power until stopped")
+            return True
+        else:
+            print("ERROR: Invalid value for power (1-100)")
+            self.stop()
+            return False
+
+    def stop(self): #to stop stirring
+        """
+        Stops fans from stirring & deinitializes pin from signal
+        """
+        
+        #led = Pin(25, Pin.OUT)
+        #led.value(0) #turn off light
+        
+        self.fans_pwm.duty_u16(0) #duty cycle = 0
+        time.sleep_ms(5) #wait for it to stop
+        self.fans_pwm.deinit()
+        self.initialized = False
+        print("Fans stopped and deinitialized")
+
+# General methods -- to be called using controller
+def initialize_and_stir(power, duration):
     """Initializes fans, stirs for designated amount of time at specified power and de-initializes fans at the end.
     
     Parameters:
@@ -54,21 +109,24 @@ def initialize_and_stir(pin_num, power, duration):
     power(int): 1-100, for controlling stirring speed (minimum 45 to turn on all fans)
     duration (float): stirring duration in MINUTES
     """
-    fans = initialize_fans(pin_num)
-    stir(fans, power=power, duration=duration)
-    stop(fans)
-    fans.deinit()
-    print("Fan initialization & stirring complete")   
+    s = StirringModule()
+    status = s.stir_for_duration(power=power, duration=duration)
+    if status:
+        print(f"Fan stirring at {power}% power for {duration} minutes complete")
+    else:
+        print("Invalid input for stirring")
 
-#Uncomment the following lines if you are uploading the file to the RPi and running it through REPL
-# try:
-#     fans = initialize_fans(0) #initialize fans at GPIO pin 0
-#     stir(fans, power=60, duration=0.2) #edit power & duration values as desired
-#     stop(fans) #stop fans
-#     fans.deinit() #deinitialize PWM pin
-#     print("Stirring Complete")
+def controller_stir(power):
+    s = StirringModule()
+    s.stir(power)
 
-# except KeyboardInterrupt: #stops fans when ctrl+c is pressed
-#     stop(fans) 
-#     fans.deinit()
-#     print("Stirring stopped")
+def controller_stop():
+    s = StirringModule()
+    s.stop()
+
+#import machine
+#led = machine.Pin("LED", machine.Pin.OUT)
+#led.value(True)
+#time.sleep(5)
+#led.value(False)
+#print("LED should be on")
